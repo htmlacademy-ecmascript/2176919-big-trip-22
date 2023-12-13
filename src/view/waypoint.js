e7296fc904a9b8e45243702cb09260e8065eba1b
Floias <irina.butcher@gmail.com>
2023-12-13 14:32:55 +0500

    исправляет отображение иконок

--- src/view/waypoint.js
+++ src/view/waypoint.js
@@ -6,7 +6,7 @@ function createWaypointTemplate(waypoint) {
   return (`<div class="event">
   <time class="event__date" datetime="2019-03-18">${dateFrom.toString().split(' ')[1]} ${dateFrom.getDate()}</time>
   <div class="event__type">
-    <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+    <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
   </div>
   <h3 class="event__title">${type} ${destination.name}</h3>
   <div class="event__schedule">
