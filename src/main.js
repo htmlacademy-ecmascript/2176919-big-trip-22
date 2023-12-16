bf1702ef404140790b792fd4f18a6686ae43320d
Floias <irina.butcher@gmail.com>
2023-12-17 02:03:02 +0500

    наводит порядок в main

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import TripPresenter from './presenter/trip-presenter.js';
 import WaypointModel from './model/waypoint-model.js';
-import { getRandomWaypoint, mockDestination, mockOptions } from './mock/waypoints.js';
 
 const siteFiltersElement = document.querySelector('.trip-controls__filters');
 const siteMainElement = document.querySelector('.trip-events');
 const waypointModel = new WaypointModel();
 const presenter = new TripPresenter({ headerContainer: siteFiltersElement, mainContainer: siteMainElement, waypointModel });
 presenter.init();
-//console.log(getRandomWaypoint())
+
