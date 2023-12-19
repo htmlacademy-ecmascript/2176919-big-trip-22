9b30d818e7f065fee77d49bd8f090b9581616588
Floias <irina.butcher@gmail.com>
2023-12-19 20:43:17 +0500

    добавляет кнопку

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,14 +1,16 @@
-import { render } from '../framework/render.js';
+import { render, RenderPosition } from '../framework/render.js';
 import FormEdit from '../view/form-edit.js';
 import Filters from '../view/filters.js';
 import Sorting from '../view/sorting.js';
 import Waypoint from '../view/waypoint.js';
+import ButtonNewEvent from '../view/button-new-event.js';
 export default class TripPresenter {
   #headerContainer = null;
   #mainContainer = null;
   #waypointModel = null;
   #filters = new Filters();
   #sorting = new Sorting();
+  #buttonNewEvent = new ButtonNewEvent();
   #waypoints = [];
   #formEdit = {};
   #waypoint = {};
@@ -21,23 +23,32 @@ export default class TripPresenter {
 
   init() {
     this.#waypoints = [...this.#waypointModel.waypoints];
-    this.#formEdit = new FormEdit({
-      waypoint: this.#waypoints[0],
-      offersType: this.#waypointModel.getOffersByType(this.#waypoints[0].type),
-      offers: [...this.#waypointModel.getOffersById(this.#waypoints[0].type, this.#waypoints[0].offersId)],
-      destination: this.#waypointModel.getDestinationsById(this.#waypoints[0].destination),
-      destinationAll: this.#waypointModel.destinations,
-    });
     render(this.#filters, this.#headerContainer);
     render(this.#sorting, this.#mainContainer);
-    render(this.#formEdit, this.#mainContainer);
+    render(this.#buttonNewEvent, this.#headerContainer, RenderPosition.AFTEREND);
+    this.#renderFormEdit(this.#waypoints[0]);
     for (let i = 0; i < this.#waypoints.length; i++) {
-      this.#waypoint = new Waypoint({
-        waypoint: this.#waypoints[i],
-        offers: [...this.#waypointModel.getOffersById(this.#waypoints[i].type, this.#waypoints[i].offersId)],
-        destination: this.#waypointModel.getDestinationsById(this.#waypoints[i].destination),
-      });
-      render(this.#waypoint, this.#mainContainer);
+      this.#renderWaypoint(this.#waypoints[i]);
     }
+  }
+
+  #renderWaypoint(waypoints) {
+    this.#waypoint = new Waypoint({
+      waypoint: waypoints,
+      offers: [...this.#waypointModel.getOffersById(waypoints.type, waypoints.offersId)],
+      destination: this.#waypointModel.getDestinationsById(waypoints.destination),
+    });
+    render(this.#waypoint, this.#mainContainer);
+  }
+
+  #renderFormEdit(waypoints) {
+    this.#formEdit = new FormEdit({
+      waypoint: waypoints,
+      offersType: this.#waypointModel.getOffersByType(waypoints.type),
+      offers: [...this.#waypointModel.getOffersById(waypoints.type, waypoints.offersId)],
+      destination: this.#waypointModel.getDestinationsById(waypoints.destination),
+      destinationAll: this.#waypointModel.destinations,
+    });
+    render(this.#formEdit, this.#mainContainer);
   }
 }
