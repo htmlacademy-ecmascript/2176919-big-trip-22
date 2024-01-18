89aedbd6f5ea357a41464fc225904c9389e986b0
Floias <irina.butcher@gmail.com>
2024-01-18 10:36:23 +0500

    добавляет модель фильтра

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import TripPresenter from './presenter/trip-presenter.js';
 import WaypointModel from './model/waypoint-model.js';
 import OffersModel from './model/offers-model.js';
 import DestinationModel from './model/destination-model.js';
+import FilterModel from './model/filter-model.js';
 
 const siteFiltersElement = document.querySelector('.trip-main__trip-controls');
 const siteMainElement = document.querySelector('.trip-events');
 const waypointModel = new WaypointModel();
 const offersModel = new OffersModel();
 const destinationModel = new DestinationModel();
+const filterModel = new FilterModel();
 
 const presenter = new TripPresenter({ headerContainer: siteFiltersElement, mainContainer: siteMainElement, waypointModel, offersModel, destinationModel });
 presenter.init();
